4ddba188fa5dba17b130854dbbc85ad734241002
squeezer-ru <jakahariyono@gmail.com>
2025-11-01 12:47:20 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/// Local array to store todo items
+console.log('hello wold');
+function validateForm(todo, date) {
+ 
+}
+
+/// Function to add a new todo item
+function addTodo() {
+console.log('+');
+}
+
+/// Placeholder function for future feature
+function deleteTodo() {
+
+}
+
+/// Placeholder functions for future features
+function filterTodo() {
+
+}
+
+/// Function to render todo items to the DOM
+function renderTodos() {
+
+}
